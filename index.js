e31482631cf801a5a45c8524dded382654b00506
[agung permana] <[agung.ten@gmail.com]>
2022-05-27 22:40:22 +0700

    second commit edit file index.js

--- index.js
+++ index.js
@@ -2,12 +2,24 @@
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
 
+const firstUserColor = new Set(['Yellow', 'Pink', 'White', 'Purple']);
+const arrFirstUserColor = Array.from(firstUserColor);
+
+const firstUserRestaurant = new Set(['Bento', 'Sushi', 'Pancake', 'Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi']);
+const arrFirstUserRestaurant = Array.from(firstUserRestaurant);
+
+const secondUserColor = new Set(['Blue', 'Black', 'Grey'])
+const arrSecondUserColor = Array.from(secondUserColor);
+
+const secondUserRestaurant =  new Set(['Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Eggy']);
+const arrSecondUserRestaurant = Array.from(secondUserRestaurant);
+
 const firstUser = {
     name: 'Monica',
     gender: 'Female',
     age: 17,
     email: 'monica@dingdong.com',
-    favoriteColor: new Set(['Yellow', 'Pink', 'White', 'Purple']),
+    favoriteColor: arrFirstUserColor,
     isHavePet: true,
     education: [{
         name: 'SD 01', 
@@ -21,14 +33,14 @@ const firstUser = {
         name: 'SMA 03', 
         city: 'Tangerang'
     }],
-    favoriteRestaurant: new Set(['Bento', 'Sushi', 'Pancake', 'Eggy', 'Tempura', 'Bento', 'Eggy', 'Padang', 'Tteok', 'Sushi', 'Sushi'])
+    favoriteRestaurant: arrFirstUserRestaurant
 };
 const secondUser = {
     name: 'Wendy',
     gender: 'Male',
     age: 23,
     email: 'wendy@dingdong.com',
-    favoriteColor: new Set(['Blue', 'Black', 'Grey']),
+    favoriteColor: arrSecondUserColor,
     isHavePet: false,
     education: [{
         name: 'SD 02', 
@@ -46,7 +58,7 @@ const secondUser = {
         name: 'Universitas Maju',
         city: 'Tangerang'
     }],
-    favoriteRestaurant: new Set(['Tempura', 'Bento', 'Sushi', 'Pancake', 'Padang', 'Katsu', 'Geprek', 'Pancake', 'Eggy'])
+    favoriteRestaurant: arrSecondUserRestaurant
 };
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
